b5f41a5782514677c7d95464349bac64ecf3649c
Syxneet <nguyenvansy09092001@gmail.com>
2024-04-19 12:13:30 +0700

    ui slidehome

--- src/app/[lang]/(support)/ve-chung-toi/page.jsx
+++ src/app/[lang]/(support)/ve-chung-toi/page.jsx
@@ -2,6 +2,7 @@ import './styles.css'
 import AboutUs from '@/sections/about-us'
 
 export default function AboutUsPage({searchParams}) {
+  console.log(searchParams)
   const {viewport} = searchParams
   const isMobile = viewport.includes('mobile')
   return (
