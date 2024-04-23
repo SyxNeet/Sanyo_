e696ad1981565d4ba7ffe2a81917cb93ba925a24
Syxneet <nguyenvansy09092001@gmail.com>
2024-04-23 15:33:38 +0700

    get data homepage

--- src/sections/home/valueDifferent/ValueDifferent.jsx
+++ src/sections/home/valueDifferent/ValueDifferent.jsx
@@ -2,25 +2,29 @@ import Image from 'next/image'
 import React from 'react'
 import SlideValue from './SlideValue'
 
-function ValueDifferent({isMobile}) {
+function ValueDifferent({isMobile, dataValueDifferent}) {
   return (
     <section className='md:h-[76.6rem] box-container-border w-full  relative max-md:pb-[3.25rem]'>
       <div className='relative  pt-[3.19rem] md:pt-[12.63rem] md:pb-[11.69rem]'>
         <Image
-          src={!isMobile?'/images/home/giaTriKhacBiet/background.png':'/images/home/giaTriKhacBiet/backgroundMb.png'}
-          alt='bg'
+          src={
+            !isMobile
+              ? dataValueDifferent?.background?.url
+              : '/images/home/giaTriKhacBiet/backgroundMb.png'
+          }
+          alt={dataValueDifferent?.background?.alt}
           width={1920}
           height={1500}
           quality={100}
           className='absolute object-cover size-full inset-0 box-container-border'
         />
-        <h4 className='sub-heading md:pl-[10.6rem] md:mb-[1.31rem] max-md:px-[0.75rem] mb-[0.44rem]'>
-          KIẾN TẠO GIÁ TRỊ KHÁC BIỆT
-        </h4>
+        <h4
+          className='sub-heading md:pl-[10.6rem] md:mb-[1.31rem] max-md:px-[0.75rem] mb-[0.44rem]'
+          dangerouslySetInnerHTML={{__html: dataValueDifferent?.title}}
+        ></h4>
         <div className='relative md:pl-[6.31rem] md:w-[61.25rem] md:mb-[4.44rem] mb-[1.94rem] max-md:ml-[0.75rem] w-[17.4375rem]'>
           <p className=' text-grey-700 font-SVNLagu md:text-[2.5rem] text-[1.125rem] font-semibold leading-1.4'>
-            "Mong muốn của chúng tôi là bảo đảm những tiêu chuẩn cao nhất nhằm
-            phục vụ khách hàng cho mọi nhu cầu liên quan đến thang máy"
+            {dataValueDifferent?.desc}
           </p>
         </div>
         {/* slide */}
