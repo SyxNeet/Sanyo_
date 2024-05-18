a4c283c0717473f9c804ce5571b12d81d38b5651
kKao2 <thanh291100@gmail.com>
2024-05-18 21:25:36 +0700

    commit

--- src/components/gsap/GsapProvider.jsx
+++ src/components/gsap/GsapProvider.jsx
@@ -3,13 +3,14 @@
 import {gsap} from 'gsap'
 import {ScrollSmoother} from 'gsap/ScrollSmoother'
 import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import {ScrollToPlugin} from 'gsap/ScrollToPlugin'
 import {useGSAP} from '@gsap/react'
 import clsx from 'clsx'
 import {usePathname} from 'next/navigation'
 import {regDuAnUrl} from '@/lib/reg'
 import useChangePage from '@/hooks/useChangePage'
 
-gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother)
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, ScrollToPlugin)
 export const scrollSmootherConfig = {
   effects: true,
   speed: 0.8,
